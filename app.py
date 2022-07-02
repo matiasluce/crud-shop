cf6084f06798e1a0f4c94594dee04e77bbd6cae2
Matias Lucero <matiaslucero1717@gmail.com>
2022-07-02 16:32:30 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,87 @@
+from flask import Flask ,jsonify,request
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost/cshop'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Producto(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    nombre = db.Column(db.String(500))
+    precio = db.Column(db.Integer)
+    stock = db.Column(db.Integer)
+    desc = db.Column(db.String(1500))
+    imgurl = db.Column(db.String(200))
+    def __init__(self,nombre,precio,stock,desc,imgurl):
+        self.nombre = nombre
+        self.precio = precio
+        self.stock = stock
+        self.desc = desc
+        self.imgurl = imgurl
+    
+db.create_all() #Crea las tablas
+
+class ProductoSchema(ma.Schema):
+    class Meta:
+        fields=('id','nombre','precio','stock','desc','imgurl')
+
+producto_schema = ProductoSchema()
+productos_schema = ProductoSchema(many = True)
+
+@app.route('/productos',methods=['GET'])
+def get_Productos():
+    all_productos = Producto.query.all()
+    result = productos_schema.dump(all_productos)
+    return jsonify(result)
+
+@app.route('/productos/<id>',methods=['GET'])
+def get_producto(id):
+    producto=Producto.query.get(id)
+    return producto_schema.jsonify(producto)
+
+@app.route('/producto/<id>',methods=['DELETE'])
+def delete_producto(id):
+    producto=Producto.query.get(id)
+    db.session.delete(producto)
+    db.session.commit()
+    return producto_schema.jsonify(producto)
+
+@app.route('/productos', methods=['POST']) # crea ruta o endpoint
+def create_producto():
+    print(request.json)  # request.json contiene el json que envio el cliente
+    nombre=request.json['nombre']
+    precio=request.json['precio']
+    stock=request.json['stock']
+    desc=request.json['desc']
+    imgurl=request.json['imgurl']
+    new_producto=Producto(nombre,precio,stock,desc,imgurl)
+    db.session.add(new_producto)
+    db.session.commit()
+    return producto_schema.jsonify(new_producto)
+
+@app.route('/productos/<id>' ,methods=['PUT'])
+def update_producto(id):
+    producto=Producto.query.get(id)
+   
+    nombre=request.json['nombre']
+    precio=request.json['precio']
+    stock=request.json['stock']
+    desc=request.json['desc']
+    imgurl=request.json['imgurl']
+ 
+    producto.nombre=nombre
+    producto.precio=precio
+    producto.stock=stock
+    producto.desc=desc
+    producto.imgurl=imgurl
+    db.session.commit()
+    return producto_schema.jsonify(producto)
+
+if __name__=='__main__':
+    app.run(debug=True, port=5000)  
---
+++ js/create.js
@@ -0,0 +1,37 @@
+function guardar() {
+ 
+    let n = document.getElementById("txtNombre").value
+    let p = parseFloat(document.getElementById("txtPrecio").value)
+    let s = parseInt(document.getElementById("txtStock").value)
+    let d = document.getElementById("txtDesc").value
+    let i = document.getElementById("txtImg").value
+
+ 
+    let producto = {
+        nombre: n,
+        precio: p,
+        stock: s,
+        desc: d,
+        imgurl: i
+    }
+    let url = "http://localhost:5000/productos"
+    var options = {
+        body: JSON.stringify(producto),
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+       // redirect: 'follow'
+    }
+    fetch(url, options)
+    .then(function () {
+        console.log("creado")
+        alert("Grabado")
+
+        // Handle response we get from the API
+    })
+    .catch(err => {
+        //this.errored = true
+        alert("Error al grabar" )
+        console.error(err);
+    })
+}
+
